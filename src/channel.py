21a2b9a3e26c6d5a4b11b6fc73f1928021b3ad57
Yuriy Eliseev <136329249+YuriyEliseev08@users.noreply.github.com>
2023-10-27 23:18:03 +0700

    прописаны условия 2 домашнего задания

--- src/channel.py
+++ src/channel.py
@@ -1,3 +1,5 @@
+import json
+
 from googleapiclient.discovery import build
 import os
 from dotenv import load_dotenv
@@ -15,12 +17,37 @@ def get_channel(channel_id):
 class Channel:
     """Класс для ютуб-канала"""
 
+    youtube = None
+
     def __init__(self, channel_id: str) -> None:
         """Экземпляр инициализируется id канала. Дальше все данные будут подтягиваться по API."""
         self.channel_id = channel_id
         self.channel_info = get_channel(self.channel_id)
+        self.title = self.channel_info['items'][0]['snippet']['title']
+        self.descriptions = self.channel_info['items'][0]['snippet']['description']
+        self.url = 'http://www.youtube.com' + self.channel_info['items'][0]['snippet']['customUrl']
+        self.subscribers = int(self.channel_info['items'][0]['statistics']['subscriberCount'])
+        self.video_count = int(self.channel_info['items'][0]['statistics']['videoCount'])
+        self.viewCount = int(self.channel_info['items'][0]['statistics']['viewCount'])
+
+    @classmethod
+    def get_channel(cls, channel_id: str):
+        yt_channel = cls.youtube.channel().list(id=channel_id, part='snippet,statistics').execute()
+        return yt_channel
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
         print(self.channel_info)
         return self.channel_info
+
+    def to_json(self, channel_name):
+        data = {}
+        data['channel_id'] = self.channel_id
+        data['channel_title'] = self.title
+        data['channel_description'] = self.descriptions
+        data['channel_url'] = self.url
+        data['channel_subscribers'] = self.subscribers
+        data['channel_videoCount'] = self.video_count
+        data['channel_viewCount'] = self.viewCount
+        with open(f'channel_name_{channel_name}.json', 'w', encoding='UTF-8') as file:
+            json.dump(data, file, indent=2, ensure_ascii=False)
